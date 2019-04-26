c9160c13977b3248744bf6612007208a4ea9e8be
parkjihyo123 <48796853+parkjihyo123@users.noreply.github.com>
2019-04-27 01:20:39 +0700

    Add project files.

---
+++ BCDHX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BCDHX.MvcApplication" Language="C#" %>
---
+++ BCDHX/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BCDHX.Startup))]
+namespace BCDHX
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
